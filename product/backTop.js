0da83b680c47b45142f25d9d7164a47d9bb17c93
FourAex <36950763+FourAex@users.noreply.github.com>
2020-06-12 23:43:29 +0800

    Add files via upload

---
+++ product/backTop.js
@@ -0,0 +1,13 @@
+function backTop () {
+    var timer = null;
+    cancelAnimationFrame(timer);
+    timer = requestAnimationFrame(function fn(){
+        var oTop = document.body.scrollTop || document.documentElement.scrollTop;
+        if(oTop > 0){
+            document.body.scrollTop = document.documentElement.scrollTop = oTop - 50;
+            timer = requestAnimationFrame(fn);
+        }else{
+            cancelAnimationFrame(timer);
+        }
+    });
+}
